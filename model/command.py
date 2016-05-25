67340ed23397daa96cd4eced6a7f5f1886670610
Federico Ferri <federico.ferri.it@gmail.com>
2016-05-25 19:31:54 +0200

    don't clear stack if there are skipped ret params

--- model/command.py
+++ model/command.py
@@ -9,6 +9,7 @@ class Command(object):
         self.description = node.find('description')
 
         self.clear_stack_after_reading_input = True
+        self.clear_stack_before_writing_output = True
 
         help_in_args = []
         help_out_args = []
@@ -36,7 +37,9 @@ class Command(object):
         for paramNode in node.findall('return/param'):
             param = Param.factory(paramNode)
             help_out_args.append(param)
-            if param.write_out:
+            if param.skip:
+                self.clear_stack_before_writing_output = False
+            elif param.write_out:
                 self.returns.append(param)
 
         help_out_args = ','.join('%s %s' % (p.htype(), p.name) for p in help_out_args) + ('=' if help_out_args else '')
