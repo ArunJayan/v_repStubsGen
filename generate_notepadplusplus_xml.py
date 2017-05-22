325f747ca88343fc7d385d65af88659df965106e
Federico Ferri <federico.ferri.it@gmail.com>
2017-05-22 21:32:24 +0200

    split generate_notepadplusplus_stuff.py

--- generate_notepadplusplus_xml.py
+++ generate_notepadplusplus_xml.py
@@ -2,16 +2,13 @@ from sys import argv, exit
 import re
 from parse import parse
 
-if len(argv) != 4:
-    print('usage: {} <reference.xml> <out-xml-file> <out-txt-file>'.format(argv[0]))
+if len(argv) != 3:
+    print('usage: {} <reference.xml> <out-xml-file>'.format(argv[0]))
     exit(1)
 
 plugin = parse(argv[1])
 
-functions = []
-variables = []
-
-with open(argv[2], 'w') as fxml, open(argv[3], 'w') as ftxt:
+with open(argv[2], 'w') as fxml:
     for cmd in plugin.commands:
         if plugin.short_name:
             func = 'sim{}.{}'.format(plugin.short_name, cmd.name)
@@ -24,7 +21,6 @@ with open(argv[2], 'w') as fxml, open(argv[3], 'w') as ftxt:
         fxml.write('</Overload>\n')
         fxml.write('</KeyWord>\n')
         fxml.write('\n')
-        functions.append(func)
 
     for enum in plugin.enums:
         for item in enum.items:
@@ -33,8 +29,5 @@ with open(argv[2], 'w') as fxml, open(argv[3], 'w') as ftxt:
             else:
                 prefix = enum.item_prefix
             fxml.write('<KeyWord name="{}{}" func="no"/>\n'.format(prefix, item))
-            variables.append(prefix+item)
         fxml.write('\n')
-
-    ftxt.write('{}\n\n{}\n'.format(' '.join(functions), ' '.join(variables)))
 
