35eb7e2511beaee8652a4f58b01a9aa95948146f
Federico Ferri <federico.ferri.it@gmail.com>
2017-05-22 20:14:01 +0200

    add merge_xml tool

---
+++ merge_xml.py
@@ -0,0 +1,15 @@
+from lxml import etree
+from sys import argv, exit
+
+if len(argv) != 4:
+    print('usage: {} <xml1> <xml2> <output>'.format(argv[0]))
+    exit(1)
+
+with open(argv[1], 'r') as f1, open(argv[2], 'r') as f2, open(argv[3], 'w') as f3:
+    tree = map(etree.parse, (f1, f2))
+    root = map(lambda tree: tree.getroot(), tree)
+    for e in root[1]:
+        print(e)
+        root[0].append(e)
+    tree[0].write(f3, encoding='utf-8', xml_declaration=True)
+
