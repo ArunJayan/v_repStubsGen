aa477c7a809aaf8b907d5fdb2d655ab81b8af193
Federico Ferri <federico.ferri.it@gmail.com>
2017-11-30 15:40:37 +0100

    more Python3 fixes

--- merge_xml.py
+++ merge_xml.py
@@ -5,9 +5,9 @@ if len(argv) != 4:
     print('usage: {} <xml1> <xml2> <output>'.format(argv[0]))
     exit(1)
 
-with open(argv[1], 'r') as f1, open(argv[2], 'r') as f2, open(argv[3], 'w') as f3:
-    tree = map(ET.parse, (f1, f2))
-    root = map(lambda tree: tree.getroot(), tree)
+with open(argv[1], 'rb') as f1, open(argv[2], 'rb') as f2, open(argv[3], 'wb') as f3:
+    tree = list(map(ET.parse, (f1, f2)))
+    root = list(map(lambda tree: tree.getroot(), tree))
     for e in root[1]:
         root[0].append(e)
     tree[0].write(f3, encoding='utf-8', xml_declaration=True)
